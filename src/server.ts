7f4daf5277efaadeda8d283b6873e9381f87dfbc
Lucas <luking122@gmail.com>
2020-08-04 18:27:34 -0300

    Segundo dia da NLW - Backend

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,10 @@
 import express from 'express';
+import cors from 'cors';
+import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
-
-app.get('/', (request, response) => {
-
-    return response.json({message:'hello word'});
-})
-
-
+app.use(routes)
 app.listen(3333);
